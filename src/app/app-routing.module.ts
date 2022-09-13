007652f2f670fd8c54f1b5306106279498382523
camilopinheiro-clina <camilo.pinheiro@clina.care>
2022-09-12 23:57:13 -0300

    salva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingComponent } from './booking/booking.component';
+import { HostComponent } from './host/host.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HostComponent,
+  },
+  {
+    path: 'reservar', component: BookingComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,39 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { ptBrLocale } from 'ngx-bootstrap/locale';
 import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookingComponent } from './booking/booking.component';
 import { ContentComponent } from './content/content.component';
-
+import { HostComponent } from './host/host.component';
+defineLocale('pt', ptBrLocale);
+registerLocaleData(localePt);
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent
+    ContentComponent,
+    BookingComponent,
+    HostComponent
   ],
   imports: [
     TypeaheadModule,
+    BsDatepickerModule,
     BrowserAnimationsModule,
     FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "pt-BR" }, //replace "en-US" with your locale
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
